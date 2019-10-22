b84f110c375726f8ae893f7b60850a67f40b3b64
Glitch (sundhar811-advancednode-fcc) <none>
2019-10-22 07:04:01 +0000

    Set up Passport

--- server.js
+++ server.js
@@ -3,7 +3,9 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const pug         = require('pug')
+const pug = require('pug');
+const passport = require('passport');
+const session = require('express-session');
 
 const app = express();
 
@@ -12,11 +14,19 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.set('view engine', 'pug')
 
 app.route('/')
   .get((req, res) => {
-    res.render(process.cwd() + '/views/pug/index.pug');
+    res.render(process.cwd() + '/views/pug/index.pug', { title: 'Hello', message: 'Please login' });
   });
 
 app.listen(process.env.PORT || 3000, () => {
