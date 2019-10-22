93e89808d78bf4e5ce6876490d02261e6a1fac0a
Glitch (sundhar811-advancednode-fcc) <none>
2019-10-22 09:22:05 +0000

    Hashing Your Passwords

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const session = require('express-session');
 const ObjectID = require('mongodb').ObjectID;
 const mongo = require('mongodb').MongoClient;
 const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
+
 const ensureAuthenticated = (req, res, next) => {
   if (req.isAuthenticated()) return next();
   res.redirect('/');
@@ -68,7 +70,7 @@ mongo.connect(process.env.DATABASE, (err, db) => {
         console.log(`User ${username} attempted to login`);
         if (err) return done(err);
         if (!user) return done(null, false);
-        if (password !== user.password) return done(null, false);
+        if (!bcrypt.compareSync(password, user.password)) return done(null, false);
         return done(null, user);
       });
     }));
@@ -88,9 +90,10 @@ mongo.connect(process.env.DATABASE, (err, db) => {
           if(err) next(err);
           else if (user) res.redirect('/')
           else {
+            let hash = bcrypt.hashSync(req.body.password, 12);
             db.collection('users').insertOne({
               username: req.body.username,
-              password: req.body.password
+              password: hash
             }, (err, doc) => {
               if (err) res.redirect('/');
               else next(null, user);
